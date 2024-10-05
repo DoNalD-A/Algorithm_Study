0c6eacb95c9c762aaa184e9d7706457ec678f66a
DoNalD-A <thdlstjrj@naver.com>
2024-10-05 15:05:57 +0900

    최소, 최대

--- src/Baekjoon/Bronze/boj_10818/Main.java
+++ src/Baekjoon/Bronze/boj_10818/Main.java
@@ -1,11 +1,11 @@
-package Baekjoon;
+package Baekjoon.Bronze.boj_10818;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solving {
+public class Main {
     public static void main(String[] args) throws IOException {
         /*
          * 1. N개 입력받기
@@ -24,7 +24,6 @@ public class Solving {
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
 
         int max = arr[0];
         int min = arr[0];
