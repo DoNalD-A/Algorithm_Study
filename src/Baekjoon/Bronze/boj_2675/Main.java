a2a99010838dba8e939dc3b27ab5bbdb00f4bed8
DoNalD-A <thdlstjrj@naver.com>
2024-10-12 13:21:37 +0900

    문자열 반복

--- src/Baekjoon/Bronze/boj_2675/Main.java
+++ src/Baekjoon/Bronze/boj_2675/Main.java
@@ -1,11 +1,11 @@
-package Baekjoon;
+package Baekjoon.Bronze.boj_2675;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solving {
+public class Main {
     public static void main(String[] args) throws IOException {
         /*
          * 1. 정수 T 가 주어진다
