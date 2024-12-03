c1a099f750f5b7448e45fa7bba52215c0f04386b
DoNalD-A <thdlstjrj@naver.com>
2024-12-03 19:54:05 +0900

    update

--- src/Baekjoon/Main.java
+++ src/Baekjoon/Main.java
@@ -1,8 +1,7 @@
 package Baekjoon;
 
+import java.util.*;
 import java.io.IOException;
-import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) {
