78a48bb627bd08b4e6e9d654a49c341ffdfb3867
DoNalD-A <thdlstjrj@naver.com>
2025-02-15 17:53:23 +0900

    X보다 작은 수

---
+++ src/Baekjoon/Bronze/boj_10871/Main.java
@@ -0,0 +1,34 @@
+package Baekjoon.Bronze.boj_10871;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        /*
+        * 1. 수열의 수 N, 정수 X 입력받기
+        * 2. 수열을 순회하며 X보다 작은 값 StringBuilder를 사용해 출력하기
+        */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (num < X) {
+                sb.append(num).append(" ");
+            }
+        }
+        System.out.println(sb.toString().trim());
+
+
+
+    }
+}
