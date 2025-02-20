10c366be353d4f92b407c538f78f958ab35744ed
DoNalD-A <thdlstjrj@naver.com>
2025-02-20 23:15:21 +0900

    Solving update

--- src/Baekjoon/Main.java
+++ src/Baekjoon/Main.java
@@ -1,70 +1,42 @@
-//package Baekjoon;
-//
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//import java.util.*;
-//
-//public class Main {
-//    public static void main(String[] args) throws IOException {
-//
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringTokenizer st = new StringTokenizer(br.readLine());
-//
-//        // 4 2 3
-//        int N = Integer.parseInt(st.nextToken());
-//        int D = Integer.parseInt(st.nextToken());
-//        int X = Integer.parseInt(st.nextToken());
-//        int[] arr = new int[N];
-//
-//        // 3 2 4 5
-//        st = new StringTokenizer(br.readLine());
-//        for (int i = 0; i < arr.length; i++) {
-//            arr[i] = Integer.parseInt(st.nextToken());
-//        }
-//
-//
-//
-//        System.out.println(arr[D-1]*N + (X-D));
-//        Scanner sc = new Scanner(System.in);
-//
-//        int T = sc.nextInt();
-//        for (int test_case = 1; test_case <= T; test_case++) {
-//
-//            int N = sc.nextInt();
-//            int D = sc.nextInt();
-//            int X = sc.nextInt();
-//            int[] arr = new int[N];
-//
-//
-////            StringTokenizer st = new StringTokenizer(sc.next(), " ");
-////            for (int i = 0; i < arr.length; i++) {
-////                //while (st.hasMoreTokens()) {
-//////                arr[i] = Integer.parseInt(st.nextToken());
-////                arr[i] = Integer.parseInt(st.nextToken());
-//
-//            String tmp = sc.next();
-//            for (int i = 0; i < tmp.length(); i += 2) {
-//                arr[i] = Integer.parseInt(tmp.charAt(i) - '0');
-//            }
-//
-//
-//            System.out.println("#" + test_case); //arr[D - 1] * N + (X - D));
-//            System.out.println(arr[D - 1] * N + (X - D));
-//        }
-//    }
-//}
-//
-//
-//
-//
-///*
-//class Solution
-//{
-// public static void main(String args[]) throws Exception
-// {
-//   System.out.println("Hello World");
-// }
-//
-//}
-// */
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        /*
+         * 1. 통화 개수 입력받아 배열에 삽입
+         * 2. ''초' / 30(60) 값 + 1 * 요금' 비교하기*/
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+        int Y = 0;
+        int M = 0;
+
+        for (int time : arr) {
+            Y += ((time / 30) + 1) * 10;
+            M += ((time / 60) + 1) * 15;
+        }
+
+        if (Y > M) {
+            System.out.print("Y " + Y);
+        }
+        if (M > Y) {
+            System.out.print("M " + M);
+        }
+
+    }
+}
--- src/Baekjoon/Main2.java
+++ src/Baekjoon/Main2.java
@@ -1,43 +1,70 @@
-package Baekjoon;
-
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-public class Main2 {
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        for (int i = 0; i < 3; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int sum = 0;
-
-            while (st.hasMoreTokens()) {
-                sum += Integer.parseInt(st.nextToken());
-            }
-
-            System.out.println(showResult(sum));
-
-        }
-    }
-
-    private static String showResult(int sum) {
-        if (sum == 0) {
-            return "D";
-        } else if (sum == 1) {
-            return "C";
-        } else if (sum == 2) {
-            return "B";
-        } else if (sum == 3) {
-            return "A";
-        } else if (sum == 4) {
-            return "E";
-        } else {
-            return "다시 입력하시오. ";
-        }
-
-    }
-}
+//package Baekjoon;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.*;
+//
+//public class Main2 {
+//    public static void main(String[] args) throws IOException {
+//
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//        // 4 2 3
+//        int N = Integer.parseInt(st.nextToken());
+//        int D = Integer.parseInt(st.nextToken());
+//        int X = Integer.parseInt(st.nextToken());
+//        int[] arr = new int[N];
+//
+//        // 3 2 4 5
+//        st = new StringTokenizer(br.readLine());
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[i] = Integer.parseInt(st.nextToken());
+//        }
+//
+//
+//
+//        System.out.println(arr[D-1]*N + (X-D));
+//        Scanner sc = new Scanner(System.in);
+//
+//        int T = sc.nextInt();
+//        for (int test_case = 1; test_case <= T; test_case++) {
+//
+//            int N = sc.nextInt();
+//            int D = sc.nextInt();
+//            int X = sc.nextInt();
+//            int[] arr = new int[N];
+//
+//
+////            StringTokenizer st = new StringTokenizer(sc.next(), " ");
+////            for (int i = 0; i < arr.length; i++) {
+////                //while (st.hasMoreTokens()) {
+//////                arr[i] = Integer.parseInt(st.nextToken());
+////                arr[i] = Integer.parseInt(st.nextToken());
+//
+//            String tmp = sc.next();
+//            for (int i = 0; i < tmp.length(); i += 2) {
+//                arr[i] = Integer.parseInt(tmp.charAt(i) - '0');
+//            }
+//
+//
+//            System.out.println("#" + test_case); //arr[D - 1] * N + (X - D));
+//            System.out.println(arr[D - 1] * N + (X - D));
+//        }
+//    }
+//}
+//
+//
+//
+//
+///*
+//class Solution
+//{
+// public static void main(String args[]) throws Exception
+// {
+//   System.out.println("Hello World");
+// }
+//
+//}
+// */
