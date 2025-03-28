4688db37c689c40c1c5d0f2afeebab32a1b95023
DoNalD-A <thdlstjrj@naver.com>
2025-03-28 20:15:24 +0900

    프로그래머스 1일차 solved

--- src/Baekjoon/Main.java
+++ src/Baekjoon/Main.java
@@ -4,33 +4,9 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        /*
-         * 1. 문자열 'S' 입력받기
-         * 2. 크기가 26(알파벳)인 배열 선언하여 각 문자 개수 세기
-         **/
-
-        // 1
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String S = br.readLine();
-
-        // 2
-        int[] alphaCount = new int[26];
-
-        for (int i = 0; i < S.length(); i++) {
-            int tmp = S.charAt(i);
-            int index = 97;
-            for (int j = 0; j < 26; j++) {
-                if (tmp == index) {
-                    alphaCount[j]++;
-                    break;
-                }
-                index++;
-            }
-        }
 
 
-        for (int i : alphaCount) {
-            System.out.print(i + " ");
-        }
+
+
     }
 }
