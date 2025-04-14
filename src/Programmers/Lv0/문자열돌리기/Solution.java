41f3f9084bd1fda5950f486d326b69c9fbef7560
DoNalD-A <thdlstjrj@naver.com>
2025-04-14 16:59:03 +0900

    문자열 돌리기

---
+++ src/Programmers/Lv0/문자열돌리기/Solution.java
@@ -0,0 +1,19 @@
+package Programmers.Lv0.문자열돌리기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String tmp = br.readLine();
+        int count = 0;
+
+        for (int i = 0; i < tmp.length(); i++) {
+            System.out.println(tmp.charAt(count));
+            count++;
+        }
+
+    }
+}
