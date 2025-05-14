6808758e7a933cd78bb391ec6f88d3b415ca70c0
DoNalD-A <thdlstjrj@naver.com>
2025-05-14 21:15:39 +0900

    문자 리스트를 문자열로 반환하기

---
+++ src/programmers/Lv0/문자_리스트를_문자열로_반환하기/Solution.java
@@ -0,0 +1,14 @@
+package programmers.Lv0.문자_리스트를_문자열로_반환하기;
+
+class Solution {
+    public String solution(String[] arr) {
+
+        String answer = "";
+
+        for (String s : arr) {
+            answer += s;
+        }
+
+        return answer;
+    }
+}
